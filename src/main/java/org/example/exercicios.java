6030d1759afba2d848c28f2da32f434d11439bab
Jaíne Rosa <jaine.rosa_11@live.com>
2025-06-23 12:23:02 -0300

    :construction: feat (exercicios) Novo exercicio

--- src/main/java/org/example/exercicios.java
+++ src/main/java/org/example/exercicios.java
@@ -5,7 +5,22 @@ import java.util.Scanner;
 public class exercicios {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        notas(scan);
+    }
 
+    public static void notas(Scanner scan){
+
+        int nota;
+        System.out.println("Informe uma nota: ");
+        nota = scan.nextInt();
+
+        while(nota < 0 || nota >10){
+            System.out.println("Nota inválida. \nDigite uma nova nota: " );
+            nota = scan.nextInt();
+        }
+
+        System.out.println("Nota válida.\nA nota digitada foi: " + nota);
 
     }
+
 }
